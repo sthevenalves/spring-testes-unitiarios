ee47067b71a4cd2457327dcd1880268100923417
Stheven <sthevenalvess@gmail.com>
2024-03-11 16:03:07 -0300

    Initial config

---
+++ src/test/java/com/sthev/testesunitarios/TestesUnitariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sthev.testesunitarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestesUnitariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
